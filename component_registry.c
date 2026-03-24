9de0e9dc0aa829d7903e0c20dead7e0650419dd9
Lorenzo <lorenzodantonio1995@gmail.com>
2026-03-24 21:24:34 +0100

    provide inline methods for accessing component pool data

--- component_registry.c
+++ component_registry.c
@@ -1,13 +1,12 @@
 #include "component_registry.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
 struct component_registry *component_registry_new(size_t capacity) {
-  struct component_registry *registry = malloc(sizeof(*registry));
-  registry->sizes = malloc(sizeof(size_t) * capacity);
-  for (size_t i = 0; i < capacity; i++) {
-    registry->sizes[i] = SIZE_MAX;
-  }
+  struct component_registry *registry =
+      malloc(sizeof(*registry) + (sizeof(*(registry->pools)) * capacity));
+
   registry->count = 0;
   registry->capacity = capacity;
   return registry;
@@ -17,9 +16,23 @@ void component_registry_free(struct component_registry *registry) {
   free(registry);
 }
 
-int component_registry_add(struct component_registry *registry, size_t size) {
-  size_t id = registry->count;
-  registry->sizes[id] = size;
-  registry->count++;
+size_t component_registry_add(struct component_registry *registry,
+                              size_t component_size, size_t capacity) {
+  size_t id = registry->count++;
+  struct component_pool *pool = &registry->pools[id];
+  pool->component_size = component_size;
+  pool->data = malloc(component_size * capacity);
+  pool->count = 0;
+  sparse_set_init(&pool->sparse_set, capacity);
   return id;
+}
+
+void component_registry_print(struct component_registry *registry) {
+  for (size_t i = 0; i < registry->count; i++) {
+    struct component_pool pool = registry->pools[i];
+    printf("component_size:\t%zu\n", pool.component_size);
+    printf("count\t%zu\n", pool.count);
+    printf("sparse set\n");
+    sparse_set_print(pool.sparse_set);
+  }
 }
--- component_registry.h
+++ component_registry.h
@@ -1,14 +1,55 @@
 #pragma once
 
+#include "datastructure.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-struct component_registry {
-  size_t *sizes;
+struct component_pool {
   size_t count;
   size_t capacity;
+  size_t component_size;
+  struct sparse_set sparse_set;
+  void *data;
+};
+
+struct component_registry {
+  size_t count;
+  size_t capacity;
+  struct component_pool pools[];
 };
 
 struct component_registry *component_registry_new(size_t capacity);
 void component_registry_free(struct component_registry *registry);
 
-int component_registry_add(struct component_registry *registry, size_t size);
+size_t component_registry_add(struct component_registry *registry, size_t size,
+                              size_t capacity);
+
+void component_registry_print(struct component_registry *registry);
+
+static inline struct component_pool *
+component_registry_get(struct component_registry *registry,
+                       size_t component_id) {
+  return &registry->pools[component_id];
+}
+
+static inline void *component_pool_get_by_position(struct component_pool *pool,
+                                                   size_t position) {
+  return pool->data + position * pool->component_size;
+}
+
+static inline void *component_pool_get_by_entity(struct component_pool *pool,
+                                                 size_t entity) {
+  size_t pos = pool->sparse_set.sparse[entity];
+  return component_pool_get_by_position(pool, pos);
+}
+
+static inline void *component_pool_emplace(struct component_pool *pool,
+                                           size_t entity) {
+  int res = sparse_set_push(&pool->sparse_set, entity);
+  if (res == -1) {
+    fprintf(stderr, "error mapping entity to sparse set\n");
+    return NULL;
+  }
+
+  return component_pool_get_by_position(pool, pool->count++);
+}
